1af92d80d7b37fcacc890cc17688ad04e94e9ba5
Talkraicer <talkraicer@gmail.com>
2025-02-21 14:38:52 +0200

    roundabout

--- Simulation/SUMO/TL_policy.py
+++ Simulation/SUMO/TL_policy.py
@@ -5,11 +5,19 @@ def sigmoid(x):
     """ Sigmoid function"""
     return 1 / (1 + np.exp(-x))
 
+#
+# def determine_policy(delay_sum, norm_factor=2.5e5):
+#     """ Determine the policy based on the delay sum"""
+#     p = sigmoid(55 * delay_sum / norm_factor - 3)
+#     prob_0 = 1 - p
+#     prob_1 = p - p ** 2
+#     prob_2 = p ** 2
+#     return np.random.choice([0, 1, 2], p=[prob_0, prob_1, prob_2])
+
 
 def determine_policy(delay_sum, norm_factor=2.5e5):
     """ Determine the policy based on the delay sum"""
     p = sigmoid(55 * delay_sum / norm_factor - 3)
     prob_0 = 1 - p
-    prob_1 = p - p ** 2
-    prob_2 = p ** 2
-    return np.random.choice([0, 1, 2], p=[prob_0, prob_1, prob_2])
+    prob_3 = p
+    return np.random.choice([0, 3], p=[prob_0, prob_3])
